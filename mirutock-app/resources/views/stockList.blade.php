eae68e322cbf8c5f8fcbe2e0cd14f84782e03ca3
kaxia7216 <kaxia.khlove@gmail.com>
2024-05-05 01:19:12 +0900

    stockページ、css、jsファイルの作成 ヘッダーまで作成済み

---
+++ mirutock-app/resources/views/stockList.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/stylesheet.css" type="text/css">
+  <title>Mirutock</title>
+</head>
+<body>
+  <div class='main'>
+  <header>
+    <div class='header-left'>
+      <a href="/">Mirutock</a>
+    </div>
+    <div class='header-right'>
+      <a href="#">logout</a>
+    </div>
+  </header>
+  <footer></footer>
+  </div>
+<script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
