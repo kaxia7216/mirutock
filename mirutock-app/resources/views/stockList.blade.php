633738672f55ec540e99ea258d3ea9a9df08e104
kaxia7216 <kaxia.khlove@gmail.com>
2024-05-08 20:25:52 +0900

    stockページ全体、モーダルは未作成

--- mirutock-app/resources/views/stockList.blade.php
+++ mirutock-app/resources/views/stockList.blade.php
@@ -61,7 +61,7 @@
               <p>牛乳</p>
             </div>
             <div class='list-center'>
-              <p>1パック</p>
+              <p>残り1パック</p>
               <p>あと3日</p>
             </div>
             <div class='list-right'>
@@ -84,18 +84,24 @@
       </div>
     </div>
   <footer>
+    <div class='link-icons'>
       <a href="#">
-        <img src="" alt="">
+        <img src="/img/shoplist.svg" alt="shoplist">
+        <!-- <span>買い物リスト</span> -->
       </a>
       <a href="#">
-        <img src="" alt="">
+        <img src="/img/fridge_all.svg" alt="fridge_all">
+        <!-- <p>全体</p> -->
       </a>
       <a href="#">
-        <img src="" alt="">
+        <img src="/img/fridge_cold.svg" alt="fridge_cold">
+        <!-- <span>冷蔵</span> -->
       </a>
       <a href="#">
-        <img src="/img/fridge_ice.svg" alt="">
+        <img src="/img/fridge_ice.svg" alt="fridge_ice">
+        <!-- <span>冷凍</span> -->
       </a>
+    </div>
   </footer>
 <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
 </body>
