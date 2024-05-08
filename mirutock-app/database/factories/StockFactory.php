e168c8304660c251a96b9e17e856f429abd65909
kaxia7216 <kaxia.khlove@gmail.com>
2024-05-09 03:26:04 +0900

    seederの作成、ページのインデントを修正

---
+++ mirutock-app/database/factories/StockFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Stock;
+use Carbon\Carbon;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Stock>
+ */
+class StockFactory extends Factory
+{
+    protected $model = Stock::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        //type, piece, unitの生成
+        $type = $this->faker->randomElement(['cold', 'ice']);
+        $numberOfItems = $this->faker->randomNumber(2, false);
+        $piece = min(10, $numberOfItems);
+        $unit = $this->faker->randomElement(['本', '個', 'パック', '袋']);
+
+        //ランダムな日付の生成
+        $startDate = Carbon::parse('-1 year'); // 過去の範囲の開始日
+        $endDate = Carbon::parse('+1 year'); // 未来の範囲の終了日
+        $randomDate = $this->faker->dateTimeBetween($startDate, $endDate)->format('Y-m-d');
+
+        return [
+            'name' => $this->faker->unique()->word,
+            'type' => $type,
+            'piece' => $piece,
+            'unit' => $unit,
+            'limit' => $randomDate,
+        ];
+    }
+}
--- mirutock-app/resources/views/stockList.blade.php
+++ mirutock-app/resources/views/stockList.blade.php
@@ -3,106 +3,106 @@
 ?>
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/css/resetCss.css" type="text/css">
-  <link rel="stylesheet" href="/css/stylesheet.css" type="text/css">
-  <title>Mirutock</title>
-</head>
-<body>
-  <header>
-      <div class='header-left'>
-        <a href="/">Mirutock</a>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/resetCss.css" type="text/css">
+    <link rel="stylesheet" href="/css/stylesheet.css" type="text/css">
+    <title>Mirutock</title>
+  </head>
+  <body>
+    <header>
+        <div class='header-left'>
+          <a href="/">Mirutock</a>
+        </div>
+        <div class='header-right'>
+          <a href="#">logout</a>
+        </div>
+    </header>
+    <div class='main-contents'>
+        <div class='list-view'>
+          <ul>
+            <li>
+              <div class='list-left'>
+                <p>いちご</p>
+              </div>
+              <div class='list-center'>
+                <p>残り1パック</p>
+                <p>あと2日</p>
+              </div>
+              <div class='list-right'>
+                <button>
+                  <img src="/img/pencil_icon.svg" alt="edit-button">
+                </button>
+                <button>
+                  <img src="/img/delete_icon.svg" alt="delete-icon">
+                </button>
+              </div>
+            </li>
+            <li>
+              <div class='list-left'>
+                <p>きゅうり</p>
+              </div>
+              <div class='list-center'>
+                <p>残り2本</p>
+                <p>あと1日</p>
+              </div>
+              <div class='list-right'>
+                <button>
+                  <img src="/img/pencil_icon.svg" alt="edit-button">
+                </button>
+                <button>
+                  <img src="/img/delete_icon.svg" alt="delete-icon">
+                </button>
+              </div>
+            </li>
+            <li>
+              <div class='list-left'>
+                <p>牛乳</p>
+              </div>
+              <div class='list-center'>
+                <p>残り1パック</p>
+                <p>あと3日</p>
+              </div>
+              <div class='list-right'>
+                <button>
+                  <img src="/img/pencil_icon.svg" alt="edit-button">
+                </button>
+                <button>
+                  <img src="/img/delete_icon.svg" alt="delete-icon">
+                </button>
+              </div>
+            </li>
+            <li>w</li>
+            <li>r</li>
+          </ul>
+        </div>
+        <div class='add-button'>
+          <a href="#">
+            <span>+</span>
+          </a>
+        </div>
       </div>
-      <div class='header-right'>
-        <a href="#">logout</a>
-      </div>
-  </header>
-  <div class='main-contents'>
-      <div class='list-view'>
-        <ul>
-          <li>
-            <div class='list-left'>
-              <p>いちご</p>
-            </div>
-            <div class='list-center'>
-              <p>残り1パック</p>
-              <p>あと2日</p>
-            </div>
-            <div class='list-right'>
-              <button>
-                <img src="/img/pencil_icon.svg" alt="edit-button">
-              </button>
-              <button>
-                <img src="/img/delete_icon.svg" alt="delete-icon">
-              </button>
-            </div>
-          </li>
-          <li>
-            <div class='list-left'>
-              <p>きゅうり</p>
-            </div>
-            <div class='list-center'>
-              <p>残り2本</p>
-              <p>あと1日</p>
-            </div>
-            <div class='list-right'>
-              <button>
-                <img src="/img/pencil_icon.svg" alt="edit-button">
-              </button>
-              <button>
-                <img src="/img/delete_icon.svg" alt="delete-icon">
-              </button>
-            </div>
-          </li>
-          <li>
-            <div class='list-left'>
-              <p>牛乳</p>
-            </div>
-            <div class='list-center'>
-              <p>残り1パック</p>
-              <p>あと3日</p>
-            </div>
-            <div class='list-right'>
-              <button>
-                <img src="/img/pencil_icon.svg" alt="edit-button">
-              </button>
-              <button>
-                <img src="/img/delete_icon.svg" alt="delete-icon">
-              </button>
-            </div>
-          </li>
-          <li>w</li>
-          <li>r</li>
-        </ul>
-      </div>
-      <div class='add-button'>
+    <footer>
+      <div class='link-icons'>
         <a href="#">
-          <span>+</span>
+          <img src="/img/shoplist.svg" alt="shoplist">
+          <!-- <span>買い物リスト</span> -->
+        </a>
+        <a href="#">
+          <img src="/img/fridge_all.svg" alt="fridge_all">
+          <!-- <p>全体</p> -->
+        </a>
+        <a href="#">
+          <img src="/img/fridge_cold.svg" alt="fridge_cold">
+          <!-- <span>冷蔵</span> -->
+        </a>
+        <a href="#">
+          <img src="/img/fridge_ice.svg" alt="fridge_ice">
+          <!-- <span>冷凍</span> -->
         </a>
       </div>
-    </div>
-  <footer>
-    <div class='link-icons'>
-      <a href="#">
-        <img src="/img/shoplist.svg" alt="shoplist">
-        <!-- <span>買い物リスト</span> -->
-      </a>
-      <a href="#">
-        <img src="/img/fridge_all.svg" alt="fridge_all">
-        <!-- <p>全体</p> -->
-      </a>
-      <a href="#">
-        <img src="/img/fridge_cold.svg" alt="fridge_cold">
-        <!-- <span>冷蔵</span> -->
-      </a>
-      <a href="#">
-        <img src="/img/fridge_ice.svg" alt="fridge_ice">
-        <!-- <span>冷凍</span> -->
-      </a>
-    </div>
-  </footer>
-<script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
-</body>
+    </footer>
+  <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+  </body>
 </html>
