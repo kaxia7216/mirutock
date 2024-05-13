61607e6691fc73de47e8b51a67cc7ab4ee60d82b
kaxia7216 <kaxia.khlove@gmail.com>
2024-05-13 16:48:46 +0900

    モーダルの共通部品化 モーダル内の入力フォームは、JavaScriptで表示内容を切り替えるようにする

--- mirutock-app/public/js/app.js
+++ mirutock-app/public/js/app.js
@@ -9,6 +9,53 @@ modalButton.addEventListener('click', function() {
   modal.style.transform = 'translateX(-50%) translateY(0)';
 
   //フォームの内容を追加する処理
+  const newCreateModal = document.getElementById('modal__content');
+  newCreateModal.innerHTML = `
+    <form action="/stock/new" method='POST' class='add-form'>
+      <fieldset>
+        @csrf
+        <legend>食材の登録</legend>
+        <fieldset>
+          <label>名前</label>
+          <input type="text" name='name'>
+        </fieldset>
+        <fieldset class='pieces'>
+          <label>個数</label>
+          <button type='button' class='decrement-button' onclick='decrementPieces()'>
+            <img src="/img/left_arrow.svg" alt="left_arrow">
+          </button>
+          <input type="text" name='piece' value='0' id='piece-number'>
+          <button type='button' class='increment-button' onclick='incrementPieces()'>
+            <img src="/img/right_arrow.svg" alt="right_arro">
+          </button>
+        </fieldset>
+        <fieldset class='set-unit'>
+          <label>単位</label>
+          <input type="text" name='unit'>
+        </fieldset>
+        <fieldset>
+          <label>保存先</label>
+          <select class='keep-select' name='select-type'>
+            <option value="" hidden>選択</option>
+            <option value="cool">冷蔵</option>
+            <option value="ice">冷凍</option>
+          </select>
+        </fieldset>
+        <fieldset>
+          <label>消費(賞味)期限</label>
+          <div class='limit-form'>
+            <input type="text" name='limit-year'>
+            <span>年</span>
+            <input type="text" name='limit-month'>
+            <span>月</span>
+            <input type="text" name='limit-day'>
+            <span>日まで</span>
+          </div>
+        </fieldset>
+        <button class='add-submit' type='submit'>追加</button>
+      </fieldset>
+    </form>
+  `;
 });
 
 //モーダルを閉じる
--- mirutock-app/resources/views/stockList.blade.php
+++ mirutock-app/resources/views/stockList.blade.php
@@ -38,60 +38,7 @@
         </div>
     </div>
     @include('layouts.footer')
-    <div class="layer editor-modal">
-      <div class="modal">
-        <div class="modal__inner">
-          <div class="modal__contents">
-            <div class="modal__content">
-              <form action="/stock/new" method='POST' class='add-form'>
-                <fieldset>
-                  @csrf
-                  <legend>食材の登録</legend>
-                  <fieldset>
-                    <label>名前</label>
-                    <input type="text" name='name'>
-                  </fieldset>
-                  <fieldset class='pieces'>
-                    <label>個数</label>
-                    <button type='button' class='decrement-button' onclick='decrementPieces()'>
-                      <img src="/img/left_arrow.svg" alt="left_arrow">
-                    </button>
-                    <input type="text" name='piece' value='0' id='piece-number'>
-                    <button type='button' class='increment-button' onclick='incrementPieces()'>
-                      <img src="/img/right_arrow.svg" alt="right_arro">
-                    </button>
-                  </fieldset>
-                  <fieldset class='set-unit'>
-                    <label>単位</label>
-                    <input type="text" name='unit'>
-                  </fieldset>
-                  <fieldset>
-                    <label>保存先</label>
-                    <select class='keep-select' name='select-type'>
-                      <option value="" hidden>選択</option>
-                      <option value="cool">冷蔵</option>
-                      <option value="ice">冷凍</option>
-                    </select>
-                  </fieldset>
-                  <fieldset>
-                    <label>消費(賞味)期限</label>
-                    <div class='limit-form'>
-                      <input type="text" name='limit-year'>
-                      <span>年</span>
-                      <input type="text" name='limit-month'>
-                      <span>月</span>
-                      <input type="text" name='limit-day'>
-                      <span>日まで</span>
-                    </div>
-                  </fieldset>
-                  <button class='add-submit' type='submit'>追加</button>
-                </fieldset>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    @include('layouts.modal')
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
   <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
   </body>
