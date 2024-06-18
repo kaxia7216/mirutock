cdf076b57c33bbc103ccb358b0166bd1feb54de4
kaxia7216 <kaxia.khlove@gmail.com>
2024-06-19 01:14:27 +0900

    自動デプロイのECR→ECSへの処理を試験的に実装 (うまくいかないようなら、コメントアウトor削除) トップページの編集(トップ画像の辺り、レイアウトまで作成済み) レスポンシブは一通り作成後に実装

---
+++ mirutock-app/public/js/top-page.js
@@ -0,0 +1,11 @@
+const topHeadTexts = document.querySelectorAll('.overlay-text p');
+
+let delay = 0;
+
+//トップページのテキストのディレイ表示
+topHeadTexts.forEach((text, index) => {
+  setTimeout(() => {
+    text.classList.add('show');
+  }, delay);
+  delay += 2000; // 2秒ごとに次のテキストを表示
+});
--- mirutock-app/resources/views/top.blade.php
+++ mirutock-app/resources/views/top.blade.php
@@ -7,13 +7,44 @@
   <body>
     @include('layouts.header-login')
     <div class='top-contents'>
-      <img src="/img/fridge_img.png" alt="fridge_img">
+      <div class="head-message">
+        <div class="head-img"></div>
+        <div class="overlay-text">
+          <p>「あ、あれ買っとくの忘れてた…」</p>
+          <p>そんなあなたのために、ストレスフリーな食材管理を実現します。</p>
+        </div>
+      </div>
+      <div class="product-discription">
+        <h2>「ミルトック」とは？</h2>
+        <p>「ミルトック」は食材管理をシンプルにするアプリです。</p>
+        <p>買い物リストも自動で作成してくれるので、余計な買い物を減らす事ができます。忙しい方にも日常に便利な食材管理アプリです。</p>
+      </div>
+      <div class="features">
+        <div class="section">
+          {{-- <img src="/img/fridge_img.png" alt="fridge_img"> --}}
+          <p>何も難しいことはありません。</p>
+          <p>今ある食材は食材リストに登録するだけ。</p>
+        </div>
+        <div class="section">
+          {{-- <img src="/img/fridge_img.png" alt="fridge_img"> --}}
+          <p>期限が近い、残り数が少ない食材は自動的に買い物リストに登録されます。</p>
+        </div>
+        <div class="section">
+          {{-- <img src="/img/fridge_img.png" alt="fridge_img"> --}}
+          <p>買い物リストにある食材を購入したら、食材リストを更新しましょう。</p>
+        </div>
+      </div>
+      <div class="start-link">
+        <p>さあ、今すぐ始めてみましょう！</p>
+        <a href="/stocks">ミルトックを使ってみる</a>
+      </div>
     </div>
+    {{-- @include('layouts.footer-login') --}}
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     @if(app()->environment('local'))
-      <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+      <script src="{{ asset('js/top-page.js') }}" type="text/javascript"></script>
     @else
-      <script src="{{ secure_asset('js/app.js') }}" type="text/javascript"></script>
+      <script src="{{ secure_asset('js/top-page.js') }}" type="text/javascript"></script>
     @endif
   </body>
 </html>
